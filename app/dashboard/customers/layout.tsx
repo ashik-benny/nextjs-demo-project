a3abe92fe789c4a60331ab0f42c7bf4b4bad3f83
ashik-benny <ashikben@newagesmb.com>
2023-11-14 12:21:05 +0530

    Done

---
+++ app/dashboard/customers/layout.tsx
@@ -0,0 +1,11 @@
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+      </div>
+      <div style={{backgroundColor:"yellow"}}>{children}</div>
+      <p>dfhgdhj</p>
+    </div>
+  );
+}
